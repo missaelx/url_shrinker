d6e88a94362a868a825b456ab9118b237dbbd81c
mhernandez <mhernandez@idisc.com>
2021-08-06 15:12:47 -0500

    Improvements for DeviceInfoMiddleware #1 - sanchezzzhak

--- src/node/middleware/DeviceInfoMiddleware.js
+++ src/node/middleware/DeviceInfoMiddleware.js
@@ -1,9 +1,9 @@
 import DeviceDetector from "node-device-detector";
 
+const detector = new DeviceDetector;
+
 export default (req, res, next) => {
-    const detector = new DeviceDetector;
     let userAgent = req.headers['user-agent'];
     req.deviceInfo = detector.detect(userAgent);
-
     next();
 }
